baeb897d7fca94e8407a076ef2d0952d97f30bf4
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-04-28 16:26:25 +0530

    Update Banner.jsx

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,28 +6,26 @@ export function Banner() {
 	const [frontClass, setFrontClass] = useState("card__side card__side--front front-1");
 	const [backClass, setBackClass] = useState("card__side card__side--back back-1");
 	// const [flipCheck, setFlipCheck]= useState(0);
-   
-   
+    let flipCheck = 1;
+    function rotateCards() {
+        if (flipCheck%2 === 0 ) {
+            setFrontClass("card__side card__side--front showGreen");
+            setBackClass("card__side card__side--back showRed");
+            // setFlipCheck(1)
+            flipCheck += 1;
+
+        } else {
+            setFrontClass("card__side card__side--front");
+            setBackClass("card__side card__side--back");
+            // setFlipCheck(0)
+            flipCheck += 1;
+        }
+		if(flipCheck < 5){
+        setTimeout(rotateCards,3500);}
+    }
 	
 
 	useEffect(() => {
-		function rotateCards() {
-			let flipCheck = 1;
-			if (flipCheck%2 === 0 ) {
-				setFrontClass("card__side card__side--front showGreen");
-				setBackClass("card__side card__side--back showRed");
-				// setFlipCheck(1)
-				flipCheck += 1;
-	
-			} else {
-				setFrontClass("card__side card__side--front");
-				setBackClass("card__side card__side--back");
-				// setFlipCheck(0)
-				flipCheck += 1;
-			}
-			if(flipCheck < 5){
-			setTimeout(rotateCards,3500);}
-		}
 		rotateCards();
 	},[]);
 
@@ -45,7 +43,7 @@ export function Banner() {
 				<div className="text-box">
 					
 				<h1 className="text-box-middle" style={{color:"#3E2723"}}>Try looking at</h1>
-					<h3 className="text-box-top"  style={{color:"#d50000"}}>POSITIVE</h3>
+					<h3 className="text-box-top"  style={{color:"#d50000", background:"#ffffff6c"}}>POSITIVE</h3>
 					<h3 className="text-box-lower" style={{color:"#3E2723"}}>---stuff through our lens---</h3>
 				</div>
 			</div>
